07a62eeb1a5a72490e5c67fa40eb86401ce7539f
pony <weixuefeng1018@gmail.com>
2025-12-26 11:37:47 +0800

    feat: update

---
+++ agent/index.ts
@@ -0,0 +1,33 @@
+function storeArrayBuffer(filename:string, buffer:ArrayBuffer) {
+    console.log('[*] Writing to file: ' + filename);
+    let destFileName:File = new File(filename, "wb");
+    
+    destFileName.write(buffer);
+    destFileName.flush();
+    destFileName.close();
+}
+
+rpc.exports = {
+    dumpProcessMemory: function (protection) {
+        let ranges:RangeDetails[] = Process.enumerateRanges(protection);
+        let totalRanges: number = ranges.length;
+        let failedDumps:number = 0;
+        console.log('[BEGIN] Located ' + totalRanges + ' memory ranges matching [' + protection + ']');
+        ranges.forEach(function (range) {
+            let destFileName = `/data/data/com.okx.wallet/cache/${range.base}_dump`;
+            let arrayBuf:ArrayBuffer|null;
+            try {
+                arrayBuf = range.base.readByteArray(range.size);
+            } catch (e) {
+                failedDumps += 1;
+                return;
+            }
+
+            if (arrayBuf) {
+                storeArrayBuffer(destFileName, arrayBuf);
+            }
+        });
+        let sucessfulDumps:number = totalRanges - failedDumps;
+        console.log(`[FINISH] Succesfully dumped ${sucessfulDumps}/${totalRanges} ranges.`);
+    },
+};
---
+++ frida-dump.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""frida-dump
+
+Tool to dump process memory matching a given PROTECTION (by default rw-),
+useful to obtain memory sections or calling it at will from a script.
+"""
+
+__author__ = 'Fernando Urbano'
+__version__ = '2020.12.1'
+__contact__ = 'fdiazmalaga@outlook.com'
+
+import os
+import sys
+
+import frida
+
+PROTECTION = 'rw-'
+
+
+def on_message(message, data):
+    print("[%s] => %s" % (message, data))
+
+
+def main(target_process):
+    session = frida.get_usb_device().attach(target_process)
+    with open('_agent.js', 'r') as agent_script:
+        contents = agent_script.read()
+    script = session.create_script(contents)
+    
+    script.on('message', on_message)
+    if not script:
+        print('Could not load _agent.js script.')
+        sys.exit(1)
+    
+    script.on('message', on_message)
+    script.load()
+    
+    try:
+        script.exports.dump_process_memory(PROTECTION)
+    except frida.InvalidOperationError:
+        print('InvalidOperationError: Process is not running anymore.')
+    session.detach()
+
+
+if __name__ == '__main__':
+    if not os.path.isfile('_agent.js'):
+        print('ERROR: Please build the agent first by running `npm run build`')
+        sys.exit(1)
+ 
+    if len(sys.argv) != 2:
+        print("Usage: %s <process name or PID> " % __file__)
+        sys.exit(1)
+    
+    try:
+        target_process = int(sys.argv[1])
+    except ValueError:
+        target_process = sys.argv[1]
+    
+    main(target_process)
